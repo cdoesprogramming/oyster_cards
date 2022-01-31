bc3182f7ba7869c5b06d1ec8452399cab482665a
camilla <cgreene553@gmail.com>
2022-01-31 17:11:24 +0000

    Completed Chapter 5: added top_up test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,10 +2,18 @@ require "Oystercard"
 
 describe Oystercard do
 it {is_expected.to respond_to :balance}
-
+it {is_expected.to respond_to(:top_up).with(1).argument}
+  
   describe "#balance" do
     it "should check a card has a balance" do
       expect(subject.balance).to eq 0 
     end
   end
+
+  describe "#top_up" do
+    it "should top up money on an oyster card" do 
+      expect{ subject.top_up(1)}.to change{ subject.balance }.by 1
+  end
+end
+
 end
